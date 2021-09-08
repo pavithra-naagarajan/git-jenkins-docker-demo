614399787b04f455b0324d891648920547cf2dc5
Pavithra  N <pavithra.n@revature.com>
2021-09-08 13:41:35 +0530

    my third commit

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Pavithra , welcome to Revature!" );
+        System.out.println( "Hello Pavithra , welcome to Revature India!" );
     }
 }
